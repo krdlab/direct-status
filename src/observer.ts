1bc093d2e23de48b9b6d2a0c34fcef64f7309c89
krdlab <krdlab@gmail.com>
2017-11-26 22:22:25 +0900

    Fix bug that returns incorrect difference data

--- src/observer.ts
+++ src/observer.ts
@@ -2,7 +2,6 @@ import * as cluster from "cluster";
 import { Worker } from "cluster";
 import { SenderProxy, ReceiverProxy, WORKER_SCRIPT } from "./client-proxy";
 import { connection } from "./db";
-import { getConnection } from "typeorm";
 import { Check } from "./entity/Check";
 import { Send } from "./entity/Send";
 import { Receive } from "./entity/Receive";
@@ -12,6 +11,10 @@ cluster.setupMaster({ exec: WORKER_SCRIPT });
 enum ServiceStatus {
   Normally  = 0,
   Degrading = 1,
+}
+
+export interface ServiceStats {
+  sendReceive: Array<{time: number, timestamp: string}>;
 }
 
 export class DirectObserverOptions {
@@ -47,18 +50,21 @@ export class DirectObserver {
     return { service: ServiceStatus.Normally }; // TODO
   }
 
-  async getServiceStats(count: number): Promise<any> { // TODO
+  // FIXME: this method is not good enough yet. hmm...
+  async getServiceStats(count: number): Promise<ServiceStats> {
+    const diff = "(strftime('%s', r.timestamp) + strftime('%f', r.timestamp) - strftime('%S', r.timestamp)) - (strftime('%s', s.timestamp) + strftime('%f', s.timestamp) - strftime('%S', s.timestamp))";
     const conn = await connection;
     const times = await conn.createQueryBuilder()
-      .select("(strftime('%f', r.timestamp) - strftime('%f', s.timestamp)) * 1000", "time")
       .from(Check, "c")
       .innerJoin(Receive, "r", "r.id = c.id")
       .innerJoin(Send, "s", "s.id = c.id")
       .orderBy("c.id", "DESC")
       .limit(count)
+      .select(`(${diff}) * 1000`, "time")
+      .addSelect("strftime('%Y-%m-%dT%H:%M:%SZ', c.timestamp)", "timestamp")
       .getRawMany();
-    const stats = {
-      sendReceive: times.map(t => t.time).reverse()
+    const stats: ServiceStats = {
+      sendReceive: times.reverse()
     };
     return stats;
   }
